a04d81746d1e2c10eb8f670257e30d0697ec18dd
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-05 14:39:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,10 +3,3 @@ def display_rainbow(colors)
 end
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-puts "the first color in the rainbow is : #{colors [0]}"
-puts "the second color in the rainbow is : #{colors [1]}"
-puts "the third color in the rainbow is : #{colors [2]}"
-puts "the fourth color in the rainbow is : #{colors [3]}"
-puts "the fifth color in the rainbow is : #{colors [4]}"
-puts "the sixth color in the rainbow is : #{colors [5]}"
-puts "the seventh color in the rainbow is : #{colors [6]}"
